c996c7bc3058340d3e4ce38377c4420373891b74
mayuribha01-code <mayuribha01@gmail.com>
2026-08-02 21:48:39 +0530

    Contest problem

---
+++ maxPairStrength.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxPairStrength(self, nums: list[int]) -> int:
+        large=0
+        for i in range (len(nums)):
+            for j in range (i+1,len(nums)):
+                if ((nums[i] * nums[j]) / math.gcd(nums[i], nums[j])**2)>large:
+                    large=(nums[i] * nums[j]) / math.gcd(nums[i], nums[j])**2
+        return int(large)
